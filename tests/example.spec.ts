82d9a0e5bb6f6f300c763db7fabab19a6c180f66
Augusto Tepedino <augusto.tepedino@gmail.com>
2026-07-13 23:00:47 -0300

    first commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,14 @@
+import 'dotenv/config'
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto(process.env.TEST_URL!);
 
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/'); 
+  await page.goto('https://playwright.dev/');
 
   await page.getByRole('link', { name: 'Get started' }).click();
 
